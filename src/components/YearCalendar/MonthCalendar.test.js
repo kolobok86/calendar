71dcab16d0970edd31439f3311e3de6f4ced93bb
с к <v.kolobkov-1986@yandex.ru>
2021-01-21 00:02:16 +0300

    react-17: add test for week starting with Monday by default

--- src/components/YearCalendar/MonthCalendar.test.js
+++ src/components/YearCalendar/MonthCalendar.test.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import MonthCalendar from './MonthCalendar';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { queries } from '@testing-library/react'
+import MonthCalendar from './MonthCalendar';
 
-describe('Month Calendar', () => {
+describe.only('Month Calendar', () => {
   it('renders the component (somehow...)', () => {
-    render(<MonthCalendar year={2020} month={1} locale="en"></MonthCalendar>)
+    render(<MonthCalendar year={2020} month={1} locale="en"></MonthCalendar>);
     expect(screen.getByText('January')).toBeDefined();
+  });
+
+  it('renders days of week', () => {
+    render(<MonthCalendar year={2020} month={1} locale="en"></MonthCalendar>)
+    expect(screen.getByText('Mon.')).toBeDefined();
+    expect(screen.getByText('Tue.')).toBeDefined();
+    expect(screen.getByText('Wed.')).toBeDefined();
+    expect(screen.getByText('Thu.')).toBeDefined();
+    expect(screen.getByText('Fri.')).toBeDefined();
+    expect(screen.getByText('Sat.')).toBeDefined();
+    expect(screen.getByText('Sun.')).toBeDefined();
+  });
+
+  it('renders days of week starting with Monday by default', () => {
+    const { container } = render(<MonthCalendar year={2020} month={1} locale="en"></MonthCalendar>);
+    const weekDaysHeader = container.querySelector('.daysOfWeek-names');
+    expect(weekDaysHeader.firstChild.textContent).toBe('Mon.');
   });
 });
